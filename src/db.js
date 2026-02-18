fd7631ef68911139e1e59611c478073dccef3c0a
jeffinzin <jefftruta0@gmail.com>
2026-02-17 22:42:42 -0300

    logica de login e register aplicada no backend

--- src/db.js
+++ src/db.js
@@ -3,47 +3,38 @@ import "dotenv/config"
 import { Client, Pool } from "pg"
 import chalk from "chalk";
 
-export async function connectDB() {
 
-    if (global.connection) {
-        return global.connection.connect();
-    }
-    // configurado o Pool
-    const pool = await new Pool({
-        connectionString: process.env.CONNECTION_STRING
-    })
+const pool = new Pool({
+    connectionString : process.env.CONNECTION_STRING
+})
 
-
-
-
-    
-    const client = await pool.connect();
-    console.log("conexão criada");
-
-    const res = await client.query("select now()")
-
-    console.log(res.rows[0]);
-    client.release();
-
-    global.connection = pool;
-    return client
+export async function query(text, params) {
+    return pool.query(text, params);
 }
 
 
-
 export async function criandotabela() {
-    const client = await connectDB();
+
     try {
         
     
-    const res = await client.query(`
+    const res = await query(`
+
     CREATE TABLE IF NOT EXISTS public.userdatas
     (
         id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
         username character varying(100) COLLATE pg_catalog."default" NOT NULL,
         password character varying(255) COLLATE pg_catalog."default",
-        CONSTRAINT userdatas_pkey PRIMARY KEY (id)
+        CONSTRAINT userdatas_pkey PRIMARY KEY (id),
+        CONSTRAINT userdatas_username UNIQUE (username),
+        CONSTRAINT userdatas_username_unique UNIQUE (username),
+        CONSTRAINT username UNIQUE (username)
     )
+
+    TABLESPACE pg_default;
+
+    ALTER TABLE IF EXISTS public.userdatas
+        OWNER to postgres;
 
         `)
         
@@ -51,5 +42,44 @@ export async function criandotabela() {
     } catch (error) {
         console.error(chalk.yellow(`erro ao executar o comando de criar tabela: ${chalk.red(error)}`))
     }
+    
+}
+
+export async function pegarusuario(usuario,senha) {
+
+    // verificando se o usuario existe no SLQ
+    try {
+    const res = await query(`
+        SELECT username,password 
+        FROM userdatas
+        WHERE username = $1 and password = $2;`, [usuario,senha])
+
+
+        if (res.rowCount === 1) {
+            // usuario ja encontrado
+            console.log(chalk.magenta("usuario ja cadastrado"));
+            console.log(res.rows);
+            return {status: "Exists"}
+            
+        }else{
+            // cadastrando usuario
+            try {
+                console.log("nao encontrada");
+                const res = await query(`INSERT INTO userdatas ("username","password") values ($1,$2)`,[usuario,senha])
+                console.log(res);
+                console.log("usuario nao indentificado no banco de dados, inserido...");    
+                return {status: "Inserted"}            
+            }                
+            catch {
+                console.log("usuario nao indentificado no banco de dados, mas um erro ocorreu (provavelmente nome ja existente)");
+                return {status: "InvalidName"}  
+            }
+        }
+    }
+    catch(erro){
+        console.log(erro);
+        return {status: "Error"}
+    }
+    
     
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// importando o express
+// importando dedpendencias
 import console from "console"
 import express from "express"
 import fs from "fs"
@@ -7,18 +7,15 @@ import "dotenv/config"
 
 // executando funções de banco de dados
 import * as db from "./db.js"
-
 db.criandotabela()
-db.connectDB()
 
-// import * as db from ".\src\db.js"
 // configurações iniciais
 const app = express();
 const PORT = process.env.PORT
 app.use(express.static("public"));
 app.use(express.json());
-
-// fazendo a primeira rota de GET
+app.use(express.urlencoded({ extended: true }));
+// iniciando rotas
 
 app.get(`/`, (req, res) =>{
     
@@ -38,7 +35,16 @@ app.get("/register", (req,res) =>{
     );
 })
 
+app.post("/register", async(req, res) =>{
+    console.log(res.body);
+    const senha = String(req.body["password"]);
+    const usuario = String(req.body["Username"]);
+    const pegarusuario = await db.pegarusuario(usuario,senha)
 
+    res.json(pegarusuario)
+
+    
+})
 
 
 // abrindo o primeiro listen para teste
